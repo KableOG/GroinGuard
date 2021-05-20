e6fd3b74feb8746246226ef709a65e8cc5496e1e
Kable <41844984+KableOG@users.noreply.github.com>
2021-05-20 15:47:11 +1000

    Update GroinGuard_Distributions.lua

--- Groin Guard/media/lua/server/Items/GroinGuard_Distributions.lua
+++ Groin Guard/media/lua/server/Items/GroinGuard_Distributions.lua
@@ -2,42 +2,42 @@ require "Items/SuburbsDistributions"
 require "Items/ProceduralDistributions"
 require "Items/ItemPicker"
 
-	table.insert(SuburbsDistributions["sportstore"]["shelves"].items, "Base.GroinGuard");
+	table.insert(SuburbsDistributions["sportstore"]["shelves"].items, "GG.GroinGuard");
 	table.insert(SuburbsDistributions["sportstore"]["shelves"].items, 10);
 
-	table.insert(SuburbsDistributions["sportstore"]["clothingrack"].items, "Base.GroinGuard");
+	table.insert(SuburbsDistributions["sportstore"]["clothingrack"].items, "GG.GroinGuard");
 	table.insert(SuburbsDistributions["sportstore"]["clothingrack"].items, 10);
 
-	table.insert(SuburbsDistributions["sportstorage"]["metal_shelves"].items, "Base.GroinGuard");
+	table.insert(SuburbsDistributions["sportstorage"]["metal_shelves"].items, "GG.GroinGuard");
 	table.insert(SuburbsDistributions["sportstorage"]["metal_shelves"].items, 10);
 
-	table.insert(SuburbsDistributions["sportstorage"]["shelves"].items, "Base.GroinGuard");
+	table.insert(SuburbsDistributions["sportstorage"]["shelves"].items, "GG.GroinGuard");
 	table.insert(SuburbsDistributions["sportstorage"]["shelves"].items, 10);
 
-	table.insert(SuburbsDistributions["sportstorage"]["clothingrack"].items, "Base.GroinGuard");
+	table.insert(SuburbsDistributions["sportstorage"]["clothingrack"].items, "GG.GroinGuard");
 	table.insert(SuburbsDistributions["sportstorage"]["clothingrack"].items, 10);
 
-	table.insert(SuburbsDistributions["changeroom"]["locker"].items, "Base.GroinGuard");
+	table.insert(SuburbsDistributions["changeroom"]["locker"].items, "GG.GroinGuard");
 	table.insert(SuburbsDistributions["changeroom"]["locker"].items, 10);
 
-	table.insert(SuburbsDistributions["bedroom"]["junk"].items, "Base.GroinGuard");
+	table.insert(SuburbsDistributions["bedroom"]["junk"].items, "GG.GroinGuard");
 	table.insert(SuburbsDistributions["bedroom"]["junk"].items, 10);
 
-	table.insert(SuburbsDistributions["Bag_Schoolbag"].items, "Base.GroinGuard");
+	table.insert(SuburbsDistributions["Bag_Schoolbag"].items, "GG.GroinGuard");
 	table.insert(SuburbsDistributions["Bag_Schoolbag"].items, 10);
 
-	table.insert(SuburbsDistributions["Bag_DuffelBag"].items, "Base.GroinGuard");
+	table.insert(SuburbsDistributions["Bag_DuffelBag"].items, "GG.GroinGuard");
 	table.insert(SuburbsDistributions["Bag_DuffelBag"].items, 10);
 	
-	table.insert(SuburbsDistributions["Bag_NormalHikingBag"].items, "Base.GroinGuard");
+	table.insert(SuburbsDistributions["Bag_NormalHikingBag"].items, "GG.GroinGuard");
 	table.insert(SuburbsDistributions["Bag_NormalHikingBag"].items, 10);
 
-	table.insert(ProceduralDistributions.list["WardrobeMan"].items, "Base.GroinGuard");
+	table.insert(ProceduralDistributions.list["WardrobeMan"].items, "GG.GroinGuard");
 	table.insert(ProceduralDistributions.list["WardrobeMan"].items, 10);
 	
-	table.insert(ProceduralDistributions.list["LockerClassy"].items, "Base.GroinGuard");
+	table.insert(ProceduralDistributions.list["LockerClassy"].items, "GG.GroinGuard");
 	table.insert(ProceduralDistributions.list["LockerClassy"].items, 10);
 	
-	table.insert(ProceduralDistributions.list["Locker"].items, "Base.GroinGuard");
+	table.insert(ProceduralDistributions.list["Locker"].items, "GG.GroinGuard");
 	table.insert(ProceduralDistributions.list["Locker"].items, 10);
 	
